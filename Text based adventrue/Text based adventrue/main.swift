7ab6cd72df609aa6a8af602d401eac8e631443cd
Ashlirankin18 <ashlirankin18@gmail.com>
2018-10-10 17:48:20 -0400

    this is the code for my project

---
+++ Text based adventrue/Text based adventrue/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Text based adventrue
+//
+//  Created by Ashli Rankin on 10/10/18.
+//  Copyright © 2018 Ashli Rankin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
