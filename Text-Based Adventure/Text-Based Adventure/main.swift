c3c287f5ee9c4a1e5d6ae861ace6dd172265b1dc
MattHuie <matthuie93@gmail.com>
2018-10-10 15:05:10 -0400

    started the project

---
+++ Text-Based Adventure/Text-Based Adventure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Text-Based Adventure
+//
+//  Created by Matthew Huie on 10/10/18.
+//  Copyright © 2018 Matthew Huie. All rights reserved.
+//
+
+import Foundation
+
+//This is my text-based adventure: Road to Canada
+
