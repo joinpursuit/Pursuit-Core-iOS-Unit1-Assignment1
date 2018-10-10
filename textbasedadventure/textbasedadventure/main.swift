78a34ade2e69bce67dcb505bb1b44315d2601353
yazzy4 <yazzy4@gmail.com>
2018-10-10 14:59:14 -0400

    First commit

---
+++ textbasedadventure/textbasedadventure/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  textbasedadventure
+//
+//  Created by Yaz Burrell on 10/10/18.
+//  Copyright © 2018 Yaz Burrell. All rights reserved.
+//
+
+import Foundation
+
+ print("Enter your option: (swim), (bike), (run)")
+//String? - optional
+if let response = readLine() { //unwraps an optional
+    
+    switch response {
+    case "swim":
+     print("dont forget your goggles")
+    case "bike":
+        print("pack an extra spare tube and hydration")
+    case "run":
+        print("nice day for a run in central park")
+    default:
+        print("no 🍺 for you")
+        print("Enter your option again????????????????????")
+        let option2 = readLine()!//force unwraps -- DANGEROUS
+        if option2 == "rest" {
+            print("happy recovery")
+        }
+    }
+}
+
