e0e9ace3bbc3a78b29a6f5e55dcd46f3c51ed070
yazzy4 <yazzy4@gmail.com>
2018-10-10 15:05:12 -0400

    First edit

--- textbasedadventure/textbasedadventure/main.swift
+++ textbasedadventure/textbasedadventure/main.swift
@@ -8,24 +8,29 @@
 
 import Foundation
 
- print("Enter your option: (swim), (bike), (run)")
-//String? - optional
-if let response = readLine() { //unwraps an optional
-    
-    switch response {
-    case "swim":
-     print("dont forget your goggles")
-    case "bike":
-        print("pack an extra spare tube and hydration")
-    case "run":
-        print("nice day for a run in central park")
-    default:
-        print("no 🍺 for you")
-        print("Enter your option again????????????????????")
-        let option2 = readLine()!//force unwraps -- DANGEROUS
-        if option2 == "rest" {
-            print("happy recovery")
-        }
-    }
-}
+// print("Enter your option: (swim), (bike), (run)")
+////String? - optional
+//if let response = readLine() { //unwraps an optional
+//
+//    switch response {
+//    case "swim":
+//     print("dont forget your goggles")
+//    case "bike":
+//        print("pack an extra spare tube and hydration")
+//    case "run":
+//        print("nice day for a run in central park")
+//    default:
+//        print("no 🍺 for you")
+//        print("Enter your option again????????????????????")
+//        let option2 = readLine()!//force unwraps -- DANGEROUS
+//        if option2 == "rest" {
+//            print("happy recovery")
+//        }
+//    }
+//}
+//
 
+var greeting = "Hi there"
+let yourName = ""
+
+print(greeting + yourName)
