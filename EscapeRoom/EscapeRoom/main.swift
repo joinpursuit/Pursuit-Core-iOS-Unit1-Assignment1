22ad03a9af053cc609c1b64ba17366ec35059974
Andréa Evans <andievans@Andreas-MacBook-Pro-3.local>
2019-10-04 16:50:13 -0400

    https://github.com/andievans-iOS/Pursuit-Core-iOS-Unit1-Assignment1

---
+++ EscapeRoom/EscapeRoom/main.swift
@@ -0,0 +1,107 @@
+//
+//  main.swift
+//  EscapeRoom
+//
+//  Created by Andréa Evans on 10/3/19.
+//  Copyright © 2019 Andrea Evans. All rights reserved.
+//
+
+
+
+
+//You are standing alone in a dimly lit walkway. Behind you is a dark wooden door, and to the North of you lies the UNKNOWN. Which way would you like to go? Choose direction "North" or "South"
+
+print("You are standing alone in a dimly lit walkway. Behind you is a dark wooden door, and to the North of you lies the UNKNOWN. Which way would you like to go? Choose direction \"North\" or \"South\"")
+ // If north: "You have decided to venture into the UNKNOWN! Enter "continue" to proceed ahead."
+// If south: "Too bad! The door is chained shut from the outside! Choose another way!"
+
+// If user inputs some other value, print: "You cannot go this way!"
+
+
+//User ultimately must input "north" or "North" and then "continue"
+
+
+// User inputs "continue"
+
+//"As you ascend toward the end of the hall, you come up to a large door."
+//User enters something like "open door" otherwise console will print "I do not recognize this entry". In case user inputs "enter room" console will print "You cannot enter room without first opening the door"
+
+//"Before you is a large room."
+//User types "enter" or "enter room"
+
+//"Boy do you have guts! You enter the room and immeditately notice a podium in the center of the space. On it, there is a small black box with gold trim and a golden lock. On either side of the box, there lies a key, each enclosed inside a glass case. Which key do you choose? Enter key 1 or key 2.
+
+//if user chooses key 1, print: "Looks like this key does not fit! Try the other!" if key 2, print: "The key fits! Enter "unlock" to see inside the box!"
+
+//User inputs "unlock"
+
+//"Inside the box there is a note."
+//User enters "read note"
+
+//NOTE: WELCOME TO THE HOUSE OF ANALIZSIA. WE ARE SO HAPPY THAT YOU HAVE ARRIVED THIS DAY! WE HAVE NOT HAD NEW GUESTS FOR MANY MOONS, SO WE ARE PLEASED FOR YOU, WE ARE!! ON THIS REST YOU MAY HAVE NOTICED,YOU HAVE, 2 KEYS. ONE KEY OPENS THIS BOX, IT DOES, AND THE OTHER YOU MUST KEEP! YOU MUST KEEP IT TO OPEN THE DOOR OF YOUR DESTINY. WE HOPE YOU STAY FOREVER INDEED! INDEED STAY FOREVER!
+
+//                          SIGNED, THE WE
+
+//User enters "close note"
+
+//"Some note! You take the key and close the box. Suddenly, before you, there appears 3 doors. You look at the key in your hand that unlocks the "DOOR TO YOUR DESTINY". Could one of these be that door?"
+//"Choose a door."
+
+//door 1 : "FLAME AND FIRE!" "YOU ARE TO BURN HERE FOR ETERNITY!" *** GAME OVER. Do you wish to play again?"
+
+
+
+//Door 2: "Before you is the garden of reflection and truth. Do you wish to Enter?"
+//                      user enters "yes" or "no"
+
+//In case of "yes" to door 2: "How serene. Up ahead you notice a path heading in either direction; one to the East, and another to the West. Which way would you like to go?"
+
+
+// If East: "Wise choice! You walk along the path and you come upon a large tree blocking your way. What do you do?"
+//              (a). go around the tree             (b). go back and take the west path                  (c). choose another door
+            
+        //In case of (a) print: "What a brave soul! Enter "continue" to move along the path.
+            //user enters "continue"
+        //"You continue along and come across an enclosure"
+        //user inputs "enter enclosure"
+        //"In the center of the enclosure, there is a single door.
+        //user inputs "open door"
+        //"DOOR IS LOCKED. YOU NEED A KEY!"
+        // user inputs "use key"
+        //"THE DOOR IS UNLOCKED. Type "enter" to enter."
+        //User types "enter"
+        //CONGRATULATIONS YOU'VE ENTERED THE LAIR OF RICHES! YOU WIN THE GAME! Do you wish to play again?
+
+            //If yes, start over, if no, GAME OVER
+        
+
+
+
+        //In case of (b) print: "You have chosen a different path. You move along West path and come to a stop at the entrance of a dark cave." Do you wish to enter?"
+            // user enters "yes" or "no"
+            // if "yes" print: "Although bravely, you have foolishly entered the lair of a giant Vugula! YOU ARE DEAD. Would you like to try again?"
+                                                        // if "no" print("GAME OVER")
+                                                        // if "yes" print: "Would you like to take the East Path or choose another door?"
+        
+      
+                    //if "take east path, print "Wise choice..." and choose (a) or (c)
+                    //if "choose another door" print: "OH NO, THE DOOR HAS DISAPPEARED! Choose east or west!
+
+        //In case of (c) print:"OH NO, THE DOOR HAS DISAPPEARED! Choose east or west!
+
+
+//If West: "You move along West path and come to a stop at the entrance of a dark cave." Do you wish to enter?"
+// user enters "yes" or "no"
+// if "yes" print: "Although bravely, you have foolishly entered the lair of a giant Vugula! YOU ARE DEAD. Would you like to try again?"
+                                            // if "no" print("GAME OVER")
+                                            // if "yes" print: "Would you like to take the East Path or choose another door?
+
+
+
+
+//Door 3: "Here lies a portal. Would you like to enter?"
+//                         user enters "yes" or "no"
+//If "yes" : "DOOR 3 IS A VOID!! YOU WILL STAY HERE WITH THE WE FOR ALL ETERNITY! CONGRATULATIONS! GAME OVER. Would you like to play again?
+
+
+print("You are standing alone in a dimly lit walkway. Behind you is a dark wooden door, and to the North of you lies the UNKNOWN. Which way would you like to go? Choose direction \"North\" or \"South\"")
